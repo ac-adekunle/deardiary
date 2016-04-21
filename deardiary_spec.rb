a8c754d8314d940f55844846cfb158a15a747d2f
ac-adekunle <lola.adekunle@gmail.com>
2016-04-20 23:29:20 -0500

    Start spec

---
+++ deardiary_spec.rb
@@ -0,0 +1,37 @@
+require_relative 'deardiary'
+
+describe Diary do 
+
+  xit 'should require a username and password' do
+  
+  end
+
+  xit 'should create a new text file' do
+  
+  end
+
+  xit 'should take user input' do
+  
+  end
+
+  xit 'should add current date to user input' do
+  
+  end
+
+  xit 'should encode user input' do
+	
+  end
+
+  xit 'should save user input and date in a text file' do
+  
+  end	
+  
+  xit 'should retrieve last 10 entries' do
+  
+  end
+
+  xit 'should decode encoded text file' do
+  
+  end
+
+end
